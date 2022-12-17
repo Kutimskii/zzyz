494cca48abafa1b1687c12e50582215bb963c07b
Kutimskii <111412701+Kutimskii@users.noreply.github.com>
2022-12-17 20:07:58 +0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -14,5 +14,9 @@ window.onscroll = function () {
   }
 };
 upButton.onclick = function () {
-  window.scrollTo(0, 0);
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });
 };
