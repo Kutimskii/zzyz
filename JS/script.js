41c4e45fa5369e6ec4813d3817a7cbfb3c437981
Kutimskii <111412701+Kutimskii@users.noreply.github.com>
2022-12-17 15:34:58 +0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,16 @@
-document.onreadystatechange = function(){
-  if(document.readyState === 'complete') {
-   let popup = document.querySelector('.popup-container');
-   let closePopup = document.querySelector('.popup-close');
-   closePopup.onclick = function () {
-     popup.classList.add('closed');
-   };
+let popup = document.querySelector('.popup-container');
+let closePopup = document.querySelector('.popup-close');
+closePopup.onclick = function () {
+  popup.classList.add('closed');
+};
+
+
+let upButton = document.querySelector('.up-button');
+window.onscroll = function () {
+  if (window.pageYOffset > 200) {
+    upButton.classList.add('shown');
   }
-}
+};
+upButton.onclick = function () {
+  window.scrollTo(0, 0);
+};
